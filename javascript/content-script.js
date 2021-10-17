5122aa0f533410f92b3094fd2699dec35b8fa37b
belabalint <b.balintbela.b@gmail.com>
2021-10-17 20:57:30 +0200

    added logic for blocking thumbnails on playlists

--- javascript/content-script.js
+++ javascript/content-script.js
@@ -1,21 +1,31 @@
-alert("working");
-var title_texts = [];
-var title_links = [];
+//find something with id video title
+//if it's a span, do x, if it's an a tag, do y
 var title_elements = document.querySelectorAll('[id="video-title"]');
 for (var i = 0; i < title_elements.length; i++) {
     var currentelement = title_elements[i];
-    var text = currentelement.attributes.getNamedItem("aria-label").value;
-    if (text[0] === 'A' || text[0] === 'a') {
-        //find rich item renderer parent and replace that obj
-        //(currentelement as any).style.display = "none";
-        while (currentelement.attributes.item(0).value != 'dismissible') {
-            currentelement = currentelement.parentElement;
+    if (currentelement.tagName === 'SPAN') {
+        //adding megyünk felfele, amedding nem ytd-playlist-renderer
+        var text = currentelement.innerText;
+        if (text[0] === 'A' || text[0] === 'a') {
+            while (currentelement.tagName !== 'YTD-PLAYLIST-RENDERER') {
+                currentelement = currentelement.parentElement;
+            }
+            while (currentelement.tagName !== "IMG") {
+                currentelement = currentelement.firstElementChild;
+            }
+            currentelement.style.display = "none";
         }
-        currentelement = currentelement.firstElementChild;
-        currentelement = currentelement.firstElementChild;
-        currentelement.style.display = "none";
+        //addig le, amig nem img
+    }
+    else if (currentelement.tagName === 'A') {
+        var text = currentelement.attributes.getNamedItem('aria-label').value;
+        if (text[0] === 'A' || text[0] === 'a') {
+            while (currentelement.attributes.item(0).value !== "dismissible") {
+                currentelement = currentelement.parentElement;
+            }
+            currentelement = currentelement.firstElementChild;
+            currentelement = currentelement.firstElementChild;
+            currentelement.style.display = "none";
+        }
     }
 }
-/*for (var i = 0; i < title_texts.length; i++) {
-    alert(title_texts[i]);
-}*/
--- source/content-script.ts
+++ source/content-script.ts
@@ -1,23 +1,30 @@
-alert("working");
-let title_texts = [];
-let title_links = [];
+//find something with id video title
+//if it's a span, do x, if it's an a tag, do y
 let title_elements = document.querySelectorAll('[id="video-title"]');
 for (var i = 0; i < title_elements.length; i++) {
     let currentelement = title_elements[i];
-    let text = currentelement.attributes.getNamedItem("aria-label").value;
-    if (text[0] === 'A' || text[0] === 'a') {
-        //find rich item renderer parent and replace that obj
-        //(currentelement as any).style.display = "none";
-        while (currentelement.attributes.item(0).value != 'dismissible') {
-            currentelement = currentelement.parentElement;
+    if (currentelement.tagName === 'SPAN') {
+        //adding megyünk felfele, amedding nem ytd-playlist-renderer
+        let text = (currentelement as any).innerText;
+        if (text[0] === 'A' || text[0] === 'a') {
+            while (currentelement.tagName !== 'YTD-PLAYLIST-RENDERER') {
+                currentelement = currentelement.parentElement;
+            }
+            while (currentelement.tagName !== "IMG") {
+                currentelement = currentelement.firstElementChild;
+            }
+            (currentelement as any).style.display = "none";
         }
-        currentelement = currentelement.firstElementChild;
-        currentelement = currentelement.firstElementChild;
-        (currentelement as any).style.display = "none";
-    }
+        //addig le, amig nem img
+    } else if (currentelement.tagName === 'A') {
+        let text = currentelement.attributes.getNamedItem('aria-label').value;
+        if (text[0] === 'A' || text[0] === 'a') {
+            while (currentelement.attributes.item(0).value !== "dismissible") {
+                currentelement = currentelement.parentElement;
+            }
+            currentelement = currentelement.firstElementChild;
+            currentelement = currentelement.firstElementChild;
+            (currentelement as any).style.display = "none";
+        }
+    } 
 }
-
-/*for (var i = 0; i < title_texts.length; i++) {
-    alert(title_texts[i]);
-}*/
-
