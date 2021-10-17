52ff6ca0709e84ed21a10f002dd167e85c828404
belabalint <b.balintbela.b@gmail.com>
2021-10-17 17:10:12 +0200

    added thumbnail blocking logic for startpage

--- javascript/content-script.js
+++ javascript/content-script.js
@@ -1,13 +1,21 @@
 alert("working");
 var title_texts = [];
 var title_links = [];
-var title_elements = document.querySelectorAll('a[id="video-title-link"]');
+var title_elements = document.querySelectorAll('[id="video-title"]');
 for (var i = 0; i < title_elements.length; i++) {
-    var currentitle = title_elements[i];
-    var text = currentitle.attributes.getNamedItem("aria-label").value;
-    title_texts.push(text);
+    var currentelement = title_elements[i];
+    var text = currentelement.attributes.getNamedItem("aria-label").value;
+    if (text[0] === 'A' || text[0] === 'a') {
+        //find rich item renderer parent and replace that obj
+        //(currentelement as any).style.display = "none";
+        while (currentelement.attributes.item(0).value != 'dismissible') {
+            currentelement = currentelement.parentElement;
+        }
+        currentelement = currentelement.firstElementChild;
+        currentelement = currentelement.firstElementChild;
+        currentelement.style.display = "none";
+    }
 }
-var image_elements = document.querySelectorAll('');
-for (var i = 0; i < title_texts.length; i++) {
-    alert(title_texts.length);
-}
+/*for (var i = 0; i < title_texts.length; i++) {
+    alert(title_texts[i]);
+}*/
--- source/content-script.ts
+++ source/content-script.ts
@@ -1,14 +1,23 @@
 alert("working");
 let title_texts = [];
 let title_links = [];
-let title_elements = document.querySelectorAll('a[id="video-title-link"]');
+let title_elements = document.querySelectorAll('[id="video-title"]');
 for (var i = 0; i < title_elements.length; i++) {
-    let currentitle = title_elements[i];
-    let text = currentitle.attributes.getNamedItem("aria-label").value;
-    title_texts.push(text);
+    let currentelement = title_elements[i];
+    let text = currentelement.attributes.getNamedItem("aria-label").value;
+    if (text[0] === 'A' || text[0] === 'a') {
+        //find rich item renderer parent and replace that obj
+        //(currentelement as any).style.display = "none";
+        while (currentelement.attributes.item(0).value != 'dismissible') {
+            currentelement = currentelement.parentElement;
+        }
+        currentelement = currentelement.firstElementChild;
+        currentelement = currentelement.firstElementChild;
+        (currentelement as any).style.display = "none";
+    }
 }
-let image_elements = document.querySelectorAll('') 
-for (var i = 0; i < title_texts.length; i++) {
-    alert(title_texts.length);
-}
+
+/*for (var i = 0; i < title_texts.length; i++) {
+    alert(title_texts[i]);
+}*/
 
