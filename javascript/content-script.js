9940d788903e0939bdf2f5f62a30e320bc36bf99
belabalint <b.balintbela.b@gmail.com>
2021-10-23 19:09:10 +0200

    added infinite scrolling blocking, replaced condition

--- javascript/content-script.js
+++ javascript/content-script.js
@@ -1,8 +1,11 @@
-var title_elements = document.querySelectorAll('[id="video-title"]');
-for (var i = 0; i < title_elements.length; i++) {
-    var currentelement = title_elements[i];
-    var text = currentelement.innerText;
-    if (text[0] === 'a' || text[0] === 'A') {
-        $(currentelement).closest('ytd-rich-item-renderer, ytd-playlist-panel-video-renderer, ytd-compact-video-renderer').find('img').hide();
+var observer = new MutationObserver(function (mutations) {
+    var title_elements = document.querySelectorAll('[id="video-title"]');
+    for (var i = 0; i < title_elements.length; i++) {
+        var currentelement = title_elements[i];
+        var text = currentelement.innerText;
+        if (text[0] != 'a' && text[0] != 'A') {
+            $(currentelement).closest('ytd-rich-item-renderer, ytd-playlist-panel-video-renderer, ytd-compact-video-renderer').find('img').hide();
+        }
     }
-}
+});
+observer.observe(document, { subtree: true, childList: true });
--- source/content-script.ts
+++ source/content-script.ts
@@ -1,8 +1,11 @@
-let title_elements = document.querySelectorAll('[id="video-title"]');
-for (var i = 0; i < title_elements.length; i++) {
-    let currentelement = title_elements[i];
-    let text = (currentelement as any).innerText;
-    if (text[0] === 'a' || text[0] === 'A') {
-        $(currentelement).closest('ytd-rich-item-renderer, ytd-playlist-panel-video-renderer, ytd-compact-video-renderer').find('img').hide();
+let observer = new MutationObserver(function(mutations) {
+    let title_elements = document.querySelectorAll('[id="video-title"]');
+    for (var i = 0; i < title_elements.length; i++) {
+        let currentelement = title_elements[i];
+        let text = (currentelement as any).innerText;
+        if (text[0] != 'a' && text[0] != 'A') {
+            $(currentelement).closest('ytd-rich-item-renderer, ytd-playlist-panel-video-renderer, ytd-compact-video-renderer').find('img').hide();
+        }
     }
-}
+});
+observer.observe(document,{subtree:true, childList:true});
