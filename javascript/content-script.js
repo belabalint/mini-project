20a911c237f28bbbfbd8ff7751b695bafded0fee
belabalint <b.balintbela.b@gmail.com>
2021-10-31 08:19:24 +0100

    fixed some cases

--- javascript/content-script.js
+++ javascript/content-script.js
@@ -1,10 +1,11 @@
+var objectstring = 'ytd-rich-item-renderer, ytd-playlist-panel-video-renderer, ytd-compact-video-renderer, ytd-video-renderer, ytd-playlist-renderer, ytd-grid-video-renderer, ytd-channel-video-player-renderer';
 var observer = new MutationObserver(function (mutations) {
     var title_elements = document.querySelectorAll('[id="video-title"]');
     for (var i = 0; i < title_elements.length; i++) {
         var currentelement = title_elements[i];
         var text = currentelement.innerText;
-        if (text[0] != 'a' && text[0] != 'A') {
-            $(currentelement).closest('ytd-rich-item-renderer, ytd-playlist-panel-video-renderer, ytd-compact-video-renderer').find('img').hide();
+        if (text[0] !== 'a' && text[0] !== 'A') {
+            $(currentelement).closest(objectstring).find('img').hide();
         }
     }
 });
--- source/content-script.ts
+++ source/content-script.ts
@@ -1,10 +1,12 @@
+let objectstring = 'ytd-rich-item-renderer, ytd-playlist-panel-video-renderer, ytd-compact-video-renderer, ytd-video-renderer, ytd-playlist-renderer, ytd-grid-video-renderer, ytd-channel-video-player-renderer';
+
 let observer = new MutationObserver(function(mutations) {
     let title_elements = document.querySelectorAll('[id="video-title"]');
     for (var i = 0; i < title_elements.length; i++) {
         let currentelement = title_elements[i];
         let text = (currentelement as any).innerText;
-        if (text[0] != 'a' && text[0] != 'A') {
-            $(currentelement).closest('ytd-rich-item-renderer, ytd-playlist-panel-video-renderer, ytd-compact-video-renderer').find('img').hide();
+        if (text[0] !== 'a' && text[0] !== 'A') {
+            $(currentelement).closest(objectstring).find('img').hide();
         }
     }
 });
